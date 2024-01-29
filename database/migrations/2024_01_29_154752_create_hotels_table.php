0bb8e1fb4166e8fc84ddc9e67552e2ddea7056e9
Eduardo Barros <eduardo@w3developer.com.br>
2024-01-29 13:35:55 -0300

    Add migrates, Models and Controllers

---
+++ database/migrations/2024_01_29_154752_create_hotels_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('hotels', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->string('address');
+            $table->string('city', 30);
+            $table->string('state', 2);
+            $table->string('zip_code', 10);
+            $table->string('website')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('hotels');
+    }
+};
